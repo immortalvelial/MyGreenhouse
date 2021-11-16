e7e5bab0b1c404867dce2384871b2c536eccf32a
immortalprogrammist <immortalvelial@icloud.com>
2021-11-16 18:53:50 +0100

    make schemaVersion

--- MyGreenhouse/Support files/AppDelegate.swift
+++ MyGreenhouse/Support files/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import RealmSwift
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -13,10 +14,29 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.makeKeyAndVisible()
         window?.rootViewController = TabBarViewController()
+        
+        changeSchemaVersion()
+        
         return true
+    }
+    
+    private func changeSchemaVersion() {
+        let schemaVersion: UInt64 = 1
+        
+        let config = Realm.Configuration(
+            schemaVersion: schemaVersion,
+            migrationBlock: { migration, oldSchemaVersion in
+                if oldSchemaVersion < schemaVersion {
+                    
+                }
+            }
+        )
+        
+        Realm.Configuration.defaultConfiguration = config
     }
 }
 
